4f521bf54a673486bdc8307ae0c8a0ce211b9630
exoen <andy_lill@hotmail.com>
2015-03-07 14:58:57 +0100

    Lagt desktop mainwindow

--- HotellDesktop/MainWindow.xaml.cs
+++ HotellDesktop/MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace HotellDesktop
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Reservasjoner reservasjoner = new Reservasjoner();
+            reservasjoner.Show();
+        }
     }
 }
