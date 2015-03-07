987b39181e57f5e9a595d2853d3513bf24145910
exoen <andy_lill@hotmail.com>
2015-03-07 16:34:19 +0100

    lagt til datacontext for view

--- HotellDesktop/MainWindow.xaml.cs
+++ HotellDesktop/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.Linq;
 
 namespace HotellDesktop
 {
@@ -20,12 +21,74 @@ namespace HotellDesktop
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        DesktopController desktopController;
+
         public MainWindow()
         {
             InitializeComponent();
+            desktopController = new DesktopController();
+            updateListView();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Updates the listview with the search criteria and date selection
+        /// </summary>
+        private void updateListView()
+        {
+           string checkedIn;
+
+                Table<HotellDLL.Booking> bookings = desktopController.getBooking();
+                if (bookings != null)
+                {
+                    var bookingslist = bookings.Select(booking => new { booking.roomId, booking.guestId, booking.checkedIn })
+                        .Join(desktopController.getGuest(), booking => booking.guestId, guest => guest.guestId, (booking, guest)
+                            => new { booking.roomId, guest.firstName, guest.lastName,checkedIn = bitToString(booking.checkedIn) })
+                            //.Join(desktopController.getService(), service => service.roomId, booking => booking.roomId, (service, booking, guest) =>
+                            //new { guest.})
+                            ;
+
+                    var list = bookingslist.Select(booking => new { booking.checkedIn, booking.firstName, booking.lastName, booking.roomId })
+                        .Join(desktopController.getService(), service => service.roomId, booking => booking.roomId,(booking,service) => 
+                            new {booking.checkedIn, booking.firstName, booking.lastName, booking.roomId, note = checkNotes(service.note)});
+
+                    listView.DataContext = list;
+                }
+          
+        }
+
+
+        private string checkNotes(String note)
+        {
+            if (note != null || note != "")
+            {
+                return "!";
+            }
+            else return "";
+        }
+
+        /// <summary>
+        /// Method for converting bit value to string. 0 return No, 1 returns Yes
+        /// </summary>
+        /// <param name="bit"> the bit to be converted</param>
+        /// <returns>No or Yes depending on the bit value</returns>
+        private string bitToString(bool bit)
+        {
+            string answer = "";
+
+            if (bit)
+            {
+                answer = "Yes";
+            }
+            else
+            {
+                answer = "No";
+            }
+
+            return answer;
+        }
+
+        private void newReservation_Click(object sender, RoutedEventArgs e)
         {
             Reservasjoner reservasjoner = new Reservasjoner();
             reservasjoner.Show();
