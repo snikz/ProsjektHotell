fe72d4b5f1a24afc91ce36cebcfb3f6a0ad99991
exoen <andy_lill@hotmail.com>
2015-03-09 07:56:07 +0100

    roomView button

--- HotellDesktop/RoomView.xaml.cs
+++ HotellDesktop/RoomView.xaml.cs
@@ -23,5 +23,25 @@ namespace HotellDesktop
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Cleares the textbox on focus so users can type their text
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NoteTextbox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            NoteTextbox.Text = "";
+        }
+
+        /// <summary>
+        /// Method for adding a note to a room
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void addNoteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
