e2bec6a1c416237ce2b70a3b2c8a11297894c440
Sigurd Reistad Christensen <pexion@gmail.com>
2015-03-11 13:26:00 +0100

    startet web-prosjektet fra scratch igjen som basic mvc

---
+++ HotellWebMvc/Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotellWebMvc.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public ActionResult Login()
+        {
+            return Content("Login");
+        }
+    }
+}
---
+++ HotellWebMvc/Controllers/BookingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotellWebMvc.Controllers
+{
+    public class BookingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("hei");
+        }
+    }
+}
