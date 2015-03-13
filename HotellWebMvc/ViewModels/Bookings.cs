1f44bcdabb6c97001b495187d3718cf288c88088
Sigurd Reistad Christensen <pexion@gmail.com>
2015-03-14 00:03:21 +0100

    added registration, display of reservation for guest, basic form for registration (not working yet)

--- HotellWebMvc/ViewModels/Bookings.cs
+++ HotellWebMvc/ViewModels/Bookings.cs
@@ -6,8 +6,15 @@ using System.Web;
 
 namespace HotellWebMvc.ViewModels
 {
+    /// <summary>
+    /// This class will prepare the different lists for the booking-view
+    /// the controller will then populate these lists and return it to the view
+    /// </summary>
+
     public class BookingIndex
     {
         public IEnumerable<Booking> Bookings { get; set; }
+        
     }
+
 }
